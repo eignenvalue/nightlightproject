4674b11d749481d7485892791c65ad215a85ad97
Sean Shangguan <sean_shangguan@sfu.ca>
2025-06-03 12:19:04 -0700

    first commit

---
+++ website/static/styles/scripts/scripts.js
@@ -0,0 +1,16 @@
+  var bslider = document.getElementById("Slider");
+    var output = document.getElementById("brightnessValue");
+    output.innerHTML = bslider.value;
+
+   bslider.oninput = function() {
+    output.innerHTML = this.value;
+   }
+
+
+        var durationinput = document.getElementById("timerInput");
+        var outputDuration = document.getElementById("durationValue");
+        outputDuration.innerHTML = timerInput.value;
+
+        timerInput.oninput = function() {
+    outputDuration.innerHTML = this.value;
+   }
